e28297ef7b37be4e43ace963a721ed7e42184dec
Ваше Имяvladimir <vladimirradochin@gmail.com>
2021-11-03 23:02:56 +0200

    Missing changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card'
+import Menu from './components/Menu';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+import OpenCard from './components/OpenCard';
+
+
+const arr=[
+  {title:'Мужские футболки вапа',price:12999, imageUrl:'/img/t_shirt.jpg'},
+  {title:'Женская футболка ципа',price:16999, imageUrl:'/img/t_shirt.jpg'},
+  ];
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div className='wrapper clear'>
+    <Drawer/>
+    
+    <Header/>
+    <OpenCard/>
+
+
+  <div className='content p-40' ></div>
+  <div className='d-flex align-center justify-between mb-40 ml-40 mr-40'>
+   <h1>VSE T-SHIRTS</h1>
+    <div className='search-block d-flex'>
+      <img src='/img/search.svg' alt='search'/>
+      <input placeholder='Поиск...'/>
+
+      </div>      
     </div>
-  );
+   
+   <div className='d-flex justify-between ml-25'  >
+     {arr.map((obj)=>(
+       
+       <Card
+       title={obj.title}
+       price={obj.price}
+       imageUrl={obj.imageUrl}
+       />
+     ))}
+       
+ 
+      
+     </div>
+   </div>
+
+
+
+
+
+      
+  
 }
 
 export default App;
